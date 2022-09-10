cc84d27fbab82ee33fd0a4b84eafef861181cb86
KnedlikMCPE <knedlik@trainmazeland.com>
2022-09-10 17:00:24 +0200

    Module selection go brrrr

--- Scripts/ModuleDropdown.gd
+++ Scripts/ModuleDropdown.gd
@@ -1,17 +1,14 @@
-extends OptionButton
+extends Control
 
 
-onready var dropdown = $ModuleDropdown
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func preview_ready():
 	var i: int = 0
 	for module in Data.unlockedModules:
-		self.add_item(module, i)
+		$ModuleDropdown.add_item(module, i)
 		i += 1
+	_on_OptionButton_item_selected(0)
 
 
 func _on_OptionButton_item_selected(index):
-	var option: String = items[index]
+	var option: String = $ModuleDropdown.items[index]
 	$ModulePreview.texture = load("res://Textures/ModulePreviews/" + option + ".png")

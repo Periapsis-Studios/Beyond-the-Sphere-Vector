e6533ecfb54a79c48e2ee0aa4a8b551fbbc72f27
KnedlikMCPE <knedlik@trainmazeland.com>
2022-12-21 14:13:23 +0100

    Planets

---
+++ Scenes/Main.gd
@@ -0,0 +1,6 @@
+extends Node2D
+class_name Main
+
+func changeToNewGame():
+	$MainMenu.free()
+	
---
+++ Scenes/UI/MainMenu.gd
@@ -0,0 +1,39 @@
+extends Control
+class_name MainMenu
+
+
+
+func _on_new_game_mouse_entered():
+	$NewGame.text = "> " + tr("newGame")
+
+func _on_new_game_mouse_exited():
+	$NewGame.text = "newGame"
+	
+
+
+func _on_load_game_mouse_entered():
+	$LoadGame.text = "> " + tr("loadGame")
+
+func _on_load_game_mouse_exited():
+	$LoadGame.text = "loadGame"
+
+
+
+func _on_settings_mouse_entered():
+	$Settings.text = "> " + tr("settings")
+
+func _on_settings_mouse_exited():
+	$Settings.text = "settings"
+
+
+
+func _on_quit_mouse_entered():
+	$Quit.text = "> " + tr("quit")
+
+func _on_quit_mouse_exited():
+	$Quit.text = "quit"
+
+
+
+func _on_quit_pressed():
+	get_tree().quit()
